88c9fc043777e202ce13f3fb3bc00ab609654e20
Mehran <36104315+Mehran-at@users.noreply.github.com>
2019-01-16 10:05:36 +0100

    Create Summarizer.java

---
+++ src/Summarizer.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+public class Summarizer {
+    private FileReader reader = new FileReader();
+    public List<String> findUniqueTricks(String filePath) {
+        return reader.asStream(filePath)
+                .map(line -> line.replaceAll("!", ""))
+                .distinct()
+                .sorted
+                .collect(Collectors.toList());
+    }
+}
+
